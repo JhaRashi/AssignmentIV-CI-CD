1503f4f658745fc77cf9cf40a01635bc2d24435b
JhaRashi <71313536+JhaRashi@users.noreply.github.com>
2024-04-01 21:57:19 +0530

    Add files via upload

---
+++ src/JavaApplicationSum.java
@@ -0,0 +1,16 @@
+package IV;
+
+import java.util.Scanner;
+
+public class JavaApplicationSum {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter Two Numbers:");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println(+(a+b));
+
+	}
+
+}
